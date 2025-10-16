824503be90608d9566c0f9e309208a2b067683ae
lemycamy <lyle.damien17.com>
2025-10-16 18:40:35 +0800

    Updated API URLs to Render backend

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://parking-zlmz.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
